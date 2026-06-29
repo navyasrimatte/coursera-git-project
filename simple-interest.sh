3ce507cf8609d0636b9a5dfd1fd6a15fba28326b
Navya <149217809+navyasrimatte@users.noreply.github.com>
2026-06-29 21:50:40 +0530

    Add simple interest calculator script

---
+++ simple-interest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Simple Interest Calculator"
+
+echo "Enter the Principal:"
+read p
+
+echo "Enter the Rate of Interest:"
+read r
+
+echo "Enter the Time Period:"
+read t
+
+si=$((p * r * t / 100))
+
+echo "Simple Interest is: $si"
